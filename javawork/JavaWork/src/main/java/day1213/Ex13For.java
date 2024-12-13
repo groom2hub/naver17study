56e1e4bc7ef258dce600e1b6b7afc20715d6bd8f
groom2hub <groomwnsgur35@gmail.com>
2024-12-13 17:34:12 +0900

    while, switch, for

---
+++ javawork/JavaWork/src/main/java/day1213/Ex13For.java
@@ -0,0 +1,33 @@
+package day1213;
+
+import java.util.Scanner;
+
+public class Ex13For {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		 * 문제 2
+		 * 5명의 나이를 입력 후
+		 * 40세 이상과 40세 미만이 각각 몇명인지 출력하고
+		 * 평균 나이도 같이 출력하시오.
+		 * (for문 사용)
+		 * */
+		
+		Scanner sc = new Scanner(System.in);
+		int n, under_40 = 0, ageSum = 0;
+		
+		
+		for (int i = 0; i < 5; i++) {
+			System.out.print("나이를 입력하시오. ");
+			n = sc.nextInt();
+			under_40 += n < 40 ? 1 : 0;
+			ageSum += n;			
+		}
+
+		System.out.printf("40세 이상 : %d명, 40세 미만 : %d명\n", 5 - under_40, under_40);
+		System.out.printf("평균 나이 : %.1f세\n", ageSum / 5.0);
+			
+	}
+
+}
