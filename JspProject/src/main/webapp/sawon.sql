54d320940b518780dccc5163244917bcc531bf4f
groom2hub <groomwnsgur35@gmail.com>
2025-01-23 17:14:58 +0900

    sawon sql

---
+++ JspProject/src/main/webapp/sawon.sql
@@ -0,0 +1,9 @@
+CREATE TABLE sawon (
+    num SMALLINT AUTO_INCREMENT PRIMARY KEY,
+    sname VARCHAR(20),
+    sphoto VARCHAR(30),
+    ipsaday VARCHAR(20),
+    sbirth SMALLINT,
+    sblood VARCHAR(3),
+    writeday DATETIME
+);
